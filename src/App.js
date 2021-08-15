fd5e66b61ec1db8b666f0868ac5f9dca7c98cf5f
Daniel Murillo Sanchez <dgerardo94@gmail.com>
2021-08-15 10:28:25 -0600

    Inicio del proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const APP_ID = 'ae22911c';
+  const APP_KEY = 'dae8c3aec6ab78b18c7a3de04d6250cb';
+
+  const [recipe, setRecipe] = useState('');
+
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  
+  useEffect(()=>{
+    getRecipes();
+  },[]);
+
+  const getRecipes = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  const updateRecipe = (e) => {
+    e.preventDefault();
+    //setRecipe(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search your recipe</h1>
+      <form onSubmit={updateRecipe} className="search-form">
+        <input type="text" className="search-bar" />
+        <button type="submit" className="search-button">Search</button>
+      </form>
     </div>
   );
 }
