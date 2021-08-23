d13fcc855ad1511d96fc6044e768f99a50d2f5d1
Daniel Murillo Sanchez <dgerardo94@gmail.com>
2021-08-23 17:21:34 -0600

    Se agregan estilos, recetas components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,69 @@
 import { useEffect, useState } from "react";
-import './App.css';
+import Recipe from "./Recipe";
+import "./App.css";
 
 function App() {
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const APP_KEY = process.env.REACT_APP_APP_KEY;
 
-  const APP_ID = 'ae22911c';
-  const APP_KEY = 'dae8c3aec6ab78b18c7a3de04d6250cb';
+  //ESTADO QUE ALMACENA LAS RECETAS
+  const [recipes, setRecipes] = useState([]);
+  //ESTADO QUE ALMACENA NUESTRA BUSQUEDA DE RECETA
+  const [search, setSearch] = useState("");
+  //ESTADO FINAL DE RECETA QUE QUEREMOS CONSULTAR
+  const [query, setQuery] = useState("chicken");
 
-  const [recipe, setRecipe] = useState('');
-
-  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-  
-  useEffect(()=>{
+  //EJECUTA OBTENER RECETAS. AL INICIO DE LA APLICARION Y CADA VEZ QUE CONSULTEMOS UNA NUEVA
+  useEffect(() => {
     getRecipes();
-  },[]);
+  }, [query]);
 
+  //OBTENER LAS RECETAS
   const getRecipes = async () => {
+    const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-  }
+    setRecipes(data.hits);
+  };
 
-  const updateRecipe = (e) => {
+  //ACTUALIZA LA BARRA DE BUSQUEDA CADA VEZ QUE DIGITEMOS
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  //ACTUALIZA LA CONSULTA A LA API PARA QUE FUNCIONE USEEFFECT
+  const getSearch = (e) => {
     e.preventDefault();
-    //setRecipe(e.target.value);
-  }
+    setQuery(search);
+    setSearch("");
+  };
 
   return (
     <div className="App">
-      <h1>Search your recipe</h1>
-      <form onSubmit={updateRecipe} className="search-form">
-        <input type="text" className="search-bar" />
-        <button type="submit" className="search-button">Search</button>
+      <div className="heading">
+        <h1>Search your recipe</h1>
+      </div>
+      <form onSubmit={getSearch} className="search-form">
+        <input
+          onChange={updateSearch}
+          value={search}
+          type="text"
+          className="search-bar"
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </form>
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={Math.random() * 1000}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 }
